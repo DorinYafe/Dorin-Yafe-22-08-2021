b11a2d5db531f24a37533a3be52ae47c8b532f86
Dorin Yafe <59007256+DorinYafe@users.noreply.github.com>
2021-08-28 21:13:41 +0300

    Update dataSource.js
    removed  unused object

--- src/dataSource/dataSource.js
+++ src/dataSource/dataSource.js
@@ -2,12 +2,6 @@ import axios from 'axios';
 import { BASE_URL, API_KEY6, API_KEY5, API_KEY4, API_KEY2 } from '../config/weatherConstants';
 import { pipe, prop, head, path, map, pick } from 'ramda';
 
-export const initialState = {
-  data: null,
-  loading: true, 
-  error: null
-};
-
 export const urls = (locationKey) => Object.freeze({
   weather: `${BASE_URL}currentconditions/v1/${locationKey}?apikey=${API_KEY2}&language=en-us`,
   fiveDaysForecast: `${BASE_URL}forecasts/v1/daily/5day/${locationKey}?apikey=${API_KEY2}&language=en-us`
